e2d93a218602bf9f19572849a179e2a3eabdf3ab
boodac <180095661+Boodac@users.noreply.github.com>
2024-09-10 15:44:29 -0400

    Add UI and support for N-ian games

--- script.js
+++ script.js
@@ -1,72 +1,97 @@
-function getComputerChoice(roll = Math.random()){
-    if(roll > (2/3)) {
-        return 'rock';
-    }
-    else if(roll > (1/3)) {
-        return 'paper'
-    }
-    else {
-        return 'scissors'
-    }
-}
-
-function getHumanChoice(input = prompt("(R)ock, (P)aper, or (S)cissors?")){
-    input.toLowerCase();
-    switch (input){
-        case 'rock':
-        case 'r':
-            return 'rock';
-        case 'paper':
-        case 'p':
-            return 'paper';
-        case 'scissors':
-        case 's':
-            return 'scissors';
-        default:
-            return getHumanChoice();
-    }
-}
-
+document.title = "Rock. Paper. Scissors.";
+const choices = ["rock", "paper", "scissors"];
+const body = document.querySelector("body");
+body.style = "display: flex;flex-direction:column;justify-content: center";
 let humanScore = 0;
 let computerScore = 0;
+let successBool = 0;
 
-function playGame(){
-    let successBool = 0;
+const container = document.createElement("div");
+container.style = "display: flex; justify-content: center; gap: 1rem;margin-top:1rem";
 
-    function playRound(computerChoice = getComputerChoice(), humanChoice = getHumanChoice()){
-        console.log(humanChoice);
-        console.log(computerChoice);    
-        switch (humanChoice){
-            case 'rock':
-                if(computerChoice==='rock') successBool = 0;
-                else if(computerChoice==='paper') successBool = -1;
-                else if(computerChoice==='scissors') successBool = 1;
-                break;
-            case 'scissors':
-                if(computerChoice==='rock') successBool = -1;
-                else if(computerChoice==='paper') successBool = 1;
-                else if(computerChoice==='scissors') successBool = 0;
-                break;
-            case 'paper':
-                if(computerChoice==='rock') successBool = 1;
-                else if(computerChoice==='paper') successBool = 0;
-                else if(computerChoice==='scissors') successBool = -1;
+// let us create the buttons
+choices.forEach((choice) => {
+    const button = document.createElement("button");
+    button.style = "padding: 1rem 2rem;border:1px solid black;border-radius:0.5rem;";
+    button.textContent = choice.charAt(0).toUpperCase()+choice.slice(1);
+    button.addClassList = choice;
+    button.addEventListener('click', () => {
+        playRound(choice);
+    })
+    container.appendChild(button);
+});
+
+const resetBtn = document.createElement("button");
+resetBtn.style = "padding: 1rem 2rem; border:1px solid black;border-radius:0.5rem;";
+resetBtn.textContent = "Reset Scores";
+resetBtn.addEventListener('click', () => {
+    reset();
+});
+body.appendChild(resetBtn);
+
+// let's create the results display
+const results = document.createElement("div");
+results.classList.add = "results";
+results.style = "display: flex; flex-direction: column; justify-content: center; text-align: center";
+
+const tally = document.createElement("p");
+tally.classList.add = "tally";
+tally.style = "text-align: center;"
+
+body.appendChild(container);
+body.appendChild(tally);
+body.appendChild(results);
+
+// returns 1 if first player won, 2 if second player won, 0 if draw
+function whoWins(first, second) {
+    let firstIndex = choices.indexOf(first);
+    let secondIndex = choices.indexOf(second);
+    if(firstIndex === secondIndex) return 0;
+    else if(firstIndex === 0 && secondIndex === choices.length-1) return 1;
+    else if(secondIndex === 0 && firstIndex === choices.length-1) return 2;
+    else if(firstIndex > secondIndex) return 1;
+    else return 2;
+}
+
+function getComputerChoice(roll = Math.random()) {
+    let compChoice = '';
+    for(let i = choices.length, j = 0;i > 0;i--,j++){
+        let comparison = ((choices.length-(j+1))/choices.length);
+        if(roll < comparison) {;
+            continue;
         }
-        
-        console.log("You chose " + humanChoice + " and they chose " + computerChoice);
-        if(successBool===-1) { console.log("You lose!"); computerScore+=1; }
-        else if (successBool===1) { console.log("You win!"); humanScore+=1; }
-        else { console.log("It's a draw!"); playRound(); }
+        else {
+            compChoice = choices[i-1];
+            break;
+        }
     }
-
-    for(let i = 0; i<5; i++){
-        playRound();
-    }
+    return compChoice;
 }
 
-playGame();
-console.log("Your score: " + humanScore + " | Their Score : " + computerScore);
-if (humanScore > computerScore) {
-    console.log("You won the game!");
+function playRound(humanChoice, computerChoice = getComputerChoice()){  
+    successBool = whoWins(humanChoice, computerChoice);
+    const result = document.createElement("p");
+    result.classList.add = "result";
+    result.textContent = "You chose " + humanChoice + " and they chose " + computerChoice;
+    if(successBool===2) computerScore+=1;
+    else if (successBool===1) humanScore+=1;
+    results.appendChild(result);
+    tally.textContent = "You: " + humanScore + " | Machine: " + computerScore;
+    if(humanScore > 4 || computerScore > 4) declareWin(successBool);
 }
-else console.log("Better luck next time. You lose.");
+
+function reset() {
+    tally.textContent = "";
+    results.textContent = "";
+    humanScore = 0;
+    computerScore = 0;
+    container.style.visibility = "visible";
+}
+
+function declareWin(player) {
+    results.textContent = "";
+    const winMessage = document.createElement("p");
+    player===1 ? winMessage.textContent = "You win!" : winMessage.textContent = "The computer wins!";
+    tally.appendChild(winMessage);
+    container.style.visibility = "hidden";
+}
