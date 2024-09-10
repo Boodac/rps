96bc246cc8fe7d40feaf3cdb3cb928917c30a87d
boodac <180095661+Boodac@users.noreply.github.com>
2024-09-10 15:59:39 -0400

    Add display for 'draws'.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-document.title = "Rock. Paper. Scissors.";
+document.title = '["rock", "paper", "scissors"]';
 const choices = ["rock", "paper", "scissors"];
 const body = document.querySelector("body");
 body.style = "display: flex;flex-direction:column;justify-content: center";
 let humanScore = 0;
 let computerScore = 0;
 let successBool = 0;
+let draws = 0;
 
 const container = document.createElement("div");
 container.style = "display: flex; justify-content: center; gap: 1rem;margin-top:1rem";
@@ -75,8 +76,9 @@ function playRound(humanChoice, computerChoice = getComputerChoice()){
     result.textContent = "You chose " + humanChoice + " and they chose " + computerChoice;
     if(successBool===2) computerScore+=1;
     else if (successBool===1) humanScore+=1;
+    else draws += 1;
     results.appendChild(result);
-    tally.textContent = "You: " + humanScore + " | Machine: " + computerScore;
+    tally.textContent = "You: " + humanScore + " | Machine: " + computerScore + " | Draws : " + draws; 
     if(humanScore > 4 || computerScore > 4) declareWin(successBool);
 }
 
@@ -85,6 +87,7 @@ function reset() {
     results.textContent = "";
     humanScore = 0;
     computerScore = 0;
+    draws = 0;
     container.style.visibility = "visible";
 }
 
